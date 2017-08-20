7a33377494825d92d39927a3f188521cbdb9fc7d
YuryMylnikov <rabota.milo@gmail.com>
2017-08-20 18:48:32 +0300

    Lecture 17

--- Z07/Z07.c
+++ Z07/Z07.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 int main(void) {
-	short mas[8] = { 9, 8, 0, 3, 2, 1, 5, 7 };
+	short mas[8] = { 1, 8, 4, 2, 0, 9, 5, 7 };
 	short rez[2][2] = { 0 };
 	short size = 10;
 	short ind = 0;
@@ -13,7 +13,7 @@ int main(void) {
 		printf("%i ", mas[i]);
 	}
 
-	for (short i = 0; i < size - 2; i++) {
+	for (short i = 0; i < size - 3; i++) {
 		flag = false;
 		ind = 0;
 		for (short k = 0; k < 2; k++) {
@@ -22,6 +22,13 @@ int main(void) {
 		}
 
 		for (short j = 0; j < size - 3; j++) {
+			if (mas[j] > mas[j + 1]) {
+				short box = mas[j];
+				mas[j] = mas[j + 1];
+				mas[j + 1] = box;
+				flag = true;
+			}
+
 			if (mas[j] < mas[j + 1] && mas[j + 1] - mas[j] > 1) {
 				rez[ind][0] = mas[j];
 				rez[ind++][1] = mas[j + 1];
@@ -30,32 +37,25 @@ int main(void) {
 					ind = 0;
 				}
 			}
-
-			if (mas[j] > mas[j + 1]) {
-				short box = mas[j];
-				mas[j] = mas[j + 1];
-				mas[j + 1] = box;
-				flag = true;
-			}
 		}
 
 		if (!flag) {
-			if (mas[0] > 0) {
-				rez[ind][0] = -1;
-				rez[ind++][1] = mas[0];
-			}
-
-			if (mas[size - 3] < 9) {
-				rez[ind][0] = mas[size - 3];
-				rez[ind++][1] = 10;
-			}
-
-			if (ind == 0) {
-				ind = 2;
-			}
-
 			break;
 		}
+	}
+
+	if (mas[0] > 0) {
+		rez[ind][0] = -1;
+		rez[ind++][1] = mas[0];
+	}
+
+	if (mas[size - 3] < 9) {
+		rez[ind][0] = mas[size - 3];
+		rez[ind++][1] = 10;
+	}
+
+	if (ind == 0) {
+		ind = 2;
 	}
 
 	printf("\nElements: ");
